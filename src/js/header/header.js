e2045da543d115720eee48b64f9bf0422a588fb5
PolinaBel <120118029+pollyBe@users.noreply.github.com>
2024-09-26 20:21:48 +0300

    feat(modules): crated modules for header components

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,11 +1,12 @@
+import { searchField } from "./searchField";
+import { cartButton, modal } from "./cart";
+
 // create structure(global)
 
 const header = document.createElement("header"),
   container = document.createElement("div"),
   headerWrap = document.createElement("div"),
-  logo = document.createElement("div"),
-  searchField = document.createElement("div"),
-  cartButton = document.createElement("button");
+  logo = document.createElement("div");
 
 // adding classes and attributes
 
@@ -23,56 +24,6 @@ logoLink.href = "#";
 logoLink.className = "logo-link";
 logoLink.textContent = "НЕ-Wildberries";
 logo.append(logoLink);
-
-// search field
-
-searchField.className = "search-field-wrap";
-
-const searchInput = document.createElement("input");
-searchInput.type = "text";
-searchInput.value = "Search ...";
-searchInput.id = "search-input";
-searchField.append(searchInput);
-
-// button for cart
-
-cartButton.id = "cart-button";
-cartButton.type = "button";
-cartButton.textContent = "Cart";
-
-// create modal cart
-
-const modal = document.createElement("div");
-modal.className = "modal";
-modal.id = "cart-modal";
-
-const modalContent = document.createElement("div");
-modalContent.className = "modal-content";
-
-const cartHead = document.createElement("div");
-cartHead.className = "cart-head";
-
-const cartTitle = document.createElement("h3");
-cartTitle.className = "cart-title";
-cartTitle.textContent = "Your Cart";
-
-const cartClear = document.createElement("button");
-cartClear.className = "cart-clear-btn";
-cartClear.textContent = "Clear";
-
-const cartStuff = document.createElement("div");
-cartStuff.className = "cart-stuff";
-
-const totalSum = document.createElement("div");
-totalSum.className = "total-sum";
-
-const totalText = document.createElement("p");
-totalText.textContent = "";
-totalSum.append(totalText);
-
-cartHead.append(cartTitle, cartClear);
-modalContent.append(cartHead, cartStuff, totalSum);
-modal.append(modalContent);
 
 // add elements in document
 
