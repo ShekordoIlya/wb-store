51800048e713656cdf8256fa0ab76646b8557ce9
PolinaBel <120118029+pollyBe@users.noreply.github.com>
2024-09-30 21:38:46 +0300

    fix(btn): fix delete item function on delete btn

--- src/js/header/cart_deleteItem.js
+++ src/js/header/cart_deleteItem.js
@@ -1,9 +1,10 @@
 import { cartAdded } from "./cart";
+import { setItemsInStorage } from "./cart_storageGetSet";
 
 export function deleteItem(e) {
   if (e.target.className === "item-delete-btn") {
-    e.target.closest(".item-content").remove();
+    e.target.closest(".item-wrap").remove();
   }
   const filter = cartAdded.filter((item) => item.id !== item.id);
-  setTodoInStorage(filter);
+  setItemsInStorage(filter);
 }
