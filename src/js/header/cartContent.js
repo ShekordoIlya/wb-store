0c3053d5589ea0dc9b5eab8365272e7659d8810f
Ilya Shekordo <27phantom03@list.ru>
2024-10-21 19:02:55 +0300

    fix(app): refactored

--- src/js/header/cartContent.js
+++ src/js/header/cartContent.js
@@ -36,27 +36,29 @@ totalSum.append(totalText);
 const paymentSite = document.createElement("div");
 paymentSite.className = "paymentSite";
 
-const paymentLink = document.createElement("a"); 
-paymentLink.textContent = "Pay";
-paymentLink.target = "_blank";  
-paymentLink.style.color = "#ffffff";        
-paymentLink.style.backgroundColor = "#007bff"; 
-paymentLink.style.padding = "10px 20px";    
-paymentLink.style.borderRadius = "5px";     
-paymentLink.style.fontSize = "16px";        
-paymentLink.style.fontWeight = "bold";      
-paymentLink.style.textAlign = "center";     
-paymentLink.style.cursor = "pointer";       
+const paymentLink = document.createElement("a");
+paymentLink.className = "payment-link";
+paymentLink.textContent = "Buy";
+paymentLink.target = "_blank";
+paymentLink.style.color = "#ffffff";
+paymentLink.style.backgroundColor = "#007bff";
+paymentLink.style.padding = "10px 20px";
+paymentLink.style.borderRadius = "5px";
+paymentLink.style.fontSize = "16px";
+paymentLink.style.fontWeight = "bold";
+paymentLink.style.textAlign = "center";
+paymentLink.style.cursor = "pointer";
 
-const pathToPayment = new URL("http://127.0.0.1:5500/src/paymentSite/index.html", window.location.href);
+const pathToPayment = new URL(
+  "http://127.0.0.1:5500/src/paymentSite/index.html",
+  window.location.href
+);
 paymentLink.href = pathToPayment.href;
 
 paymentSite.append(paymentLink);
 
-
-
 cartHead.append(cartTitle, cartClear);
-cartContent.append(cartHead, cartStuff, totalSum, paymentSite,paymentLink);
+cartContent.append(cartHead, cartStuff, totalSum, paymentSite, paymentLink);
 modalContent.append(cartContent);
 
 let cartAdded = getItemFromStorage();
